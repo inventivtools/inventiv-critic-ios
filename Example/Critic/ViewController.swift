479dc008074eca3e4ec6a48d5c54cedaa0bb7201
Dave Lane <dclane@gmail.com>
2018-02-17 00:19:52 -0600

    Updating README and example code.

--- Example/Critic/ViewController.swift
+++ Example/Critic/ViewController.swift
@@ -15,26 +15,6 @@ class ViewController: UIViewController {
 
     //MARK: Actions
     @IBAction func sendFeedbackButton() {
-        
-//        let podBundle = Bundle(for: Critic.self)
-//        let bundleURL = podBundle.url(forResource:"Critic", withExtension: "bundle")
-//        let bundle = Bundle(url: bundleURL!)!
-//        let storyboard = UIStoryboard(name: "FeedbackScreen", bundle: bundle)
-//        let vc = storyboard.instantiateViewController(withIdentifier: "FeedbackScreen")
-//        present(vc, animated: true, completion: nil)
-        Critic.instance().showDefaultFeedbackScreen(self);
-        
-        
-//        let reportCreator = NVCReportCreator()
-//        reportCreator.description = "Testing from iOS."
-//
-//        reportCreator.create({(success: Bool, error: Error?) in
-//            if success {
-//                NSLog("Feedback has been submitted!")
-//            }
-//            else {
-//                NSLog("Feedback submission failed.")
-//            }
-//        })
+        Critic.instance().showDefaultFeedbackScreen(self)
     }
 }
--- README.md
+++ README.md
@@ -18,8 +18,25 @@ pod 'Critic'
 2. Find your Product Access Token in the [Critic Web Portal](https://critic.inventiv.io/products) by viewing your Product's details.
 3. Initialize Critic by starting it from your `AppDelegate`.
 ```swift
-TODO
+Critic.instance().start("YOUR_PRODUCT_ACCESS_TOKEN")
 ```
+
+By default, devices that are not connected to a debug session will pipe console output (`stderr` and `stdout`) to a log file, which is 
+included with submitted Reports. You can disable this behavior prior to starting Critic.
+```swift
+Critic.instance().shouldLogToFile = false;
+Critic.instance().start("YOUR_PRODUCT_ACCESS_TOKEN")
+```
+
+## Sending Customer Feedback Reports
+
+You can show the default feedback report screen any time you like by calling the following method from a UIViewController.
+```swift
+Critic.instance().showDefaultFeedbackScreen(self)
+```
+
+## Customizing Feedback Reports
+
 
 ## License
 
